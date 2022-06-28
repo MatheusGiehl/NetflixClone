380df7ffeadd70ef3a7abc44036ea9bf048fd406
Matheus <giehlmatheus96@gmail.com>
2022-06-28 19:08:30 -0300

    update Featureed.js

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -1,2 +1,15 @@
-import React from "react";
+import React from 'react'
 import './FeaturedMovie.css';
+
+export default ({item}) => {
+
+    return (
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+
+        </section>
+    )
+}
