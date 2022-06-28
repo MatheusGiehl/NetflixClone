de080c41da20b8e536e77daa02d70ccbd7b4a4fa
Matheus <giehlmatheus96@gmail.com>
2022-06-28 18:27:16 -0300

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, {useState, useEffect} from "react";
 import './App.css';
-import Tmdb from "./Tmdb";
-import MovieRow from "./components/MovieRow";
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
     const [movieList, setMovieList] = useState([]);
+    const [featuredData, setFeaturedMovie] =  useState(null)
     
     useEffect(() => {
         const loadAll = async () => {
             let list = await Tmdb.getHomeList();
             setMovieList(list);
+
+            let originals = list.filter(i => i.slug === 'originals')
+            let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length -1))
+            let chosen = originals[0].items.results[randomChosen]
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+            
         }
 
         loadAll();
     }, [])
     return(
         <div className="page">
+            {featuredData &&
+            < FeaturedMovie item={featuredData} />
+            }
             <div className="lists">
                 {movieList.map((item, key) =>
                 <MovieRow key={key} title={item.title} items={item.items} />
