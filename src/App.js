19ea8d16440353f9190c0220d4ff6c5cca4c6da5
Matheus <giehlmatheus96@gmail.com>
2022-06-28 13:10:42 -0300

    update App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from "react";
+import './App.css';
+import Tmdb from "./Tmdb";
+
+export default () => {
+    const [movieList, setMovieList] = useState([]);
+    
+    useEffect(() => {
+        const loadAll = async () => {
+            let list = await Tmdb.getHomeList();
+        }
+
+        loadAll();
+    }, [])
+    return(
+        <h1>Oi</h1>
+    )
+}
