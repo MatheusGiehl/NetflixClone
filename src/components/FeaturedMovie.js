e9ca5172b7fc54e5e597945f22d4ed395c78efdf
Matheus <giehlmatheus96@gmail.com>
2022-06-28 21:02:24 -0300

    update FeaturedMovie.js

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,13 +3,27 @@ import './FeaturedMovie.css';
 
 export default ({item}) => {
 
+    let firstDate = new Date(item.first_air_date)
+
     return (
         <section className="featured" style={{
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
-
+                <div className="featured--vertical"> 
+                <div className="featured--horizontal">
+                    <div className="featured--name">
+                        {item.name}
+                    </div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average}</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                        
+                    </div>
+                </div>
+            </div> 
         </section>
     )
 }
