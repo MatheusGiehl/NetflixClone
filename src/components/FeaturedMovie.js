9f3a5cedff3e9f10ab49e25dcb9393e13c0e4988
Matheus <giehlmatheus96@gmail.com>
2022-07-01 18:32:14 -0300

    update FeaturedMovie.js

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -4,6 +4,16 @@ import './FeaturedMovie.css';
 export default ({item}) => {
 
     let firstDate = new Date(item.first_air_date)
+    let description = item.overview;
+    let genres = []
+
+    if(description.lenght > 200) {
+        description = description.substring(0, 200 + '...');
+    }
+
+    for(let indice in item.genres) {
+        genres.push(item.genres[indice].name)
+    }
 
     return (
         <section className="featured" style={{
@@ -20,7 +30,14 @@ export default ({item}) => {
                         <div className="featured--points">{item.vote_average}</div>
                         <div className="featured--year">{firstDate.getFullYear()}</div>
                         <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
-                        
+                    </div>
+                    <div className="featured--description">{description}</div>
+                    <div className="featured--buttons">
+                        <a href={`/watch/${item.id}`} className="featured--watchbtn">Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="featured--listbtn">Minha Lista</a>
+                    </div>
+                    <div className="featured--genres">
+                        <strong>Gêneros: </strong> {genres.join(',') }
                     </div>
                 </div>
             </div> 
