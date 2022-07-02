d06d37933141ff0dda2def21b73f6fe5c53da360
Matheus <giehlmatheus96@gmail.com>
2022-07-01 21:38:22 -0300

    update MovieRow.js

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import './MovieRow.css'
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 
 export default ({title, items}) => {
     const [scrollX, setScrollX] = useState(0);
@@ -23,8 +25,13 @@ export default ({title, items}) => {
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+            <div className="movieRow--left" onClick={handleLeftArrow}> <NavigateBeforeIcon style={{fontSize: 50}}/> </div>
+            <div className="movieRow--right" onClick={handleRigthArrow}> <NavigateNextIcon style={{fontSize: 50}}/> </div>
             <div className="movieRow--listarea">
-                <div className="movieRow--list" style={{width : 10000}}>
+                <div className="movieRow--list" style={{
+                    marginLeft: scrollX,
+                    width: items.results.length * 150   
+                }}>
             {items.results.length > 0 && items.results.map((item, key) => (
                 <div className="movieRow--item" key="key">
                     <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.orginal_title} />
