3cae9b7af768ef46a30c0346b2497c7ca328d093
Matheus <giehlmatheus96@gmail.com>
2022-07-01 21:37:24 -0300

    update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '.Header.css'
+import './Header.css'
 
 export default ({ black }) => {
     return (
@@ -8,12 +8,12 @@ export default ({ black }) => {
                 <a href="/">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"  alt="Netlix"/>
                 </a>
+                </div>
                 <div className="header-user">
                     <a href="/">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" />
                     </a>
                 </div>
-            </div>
         </header>
     )
 }
